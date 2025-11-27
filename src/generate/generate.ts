34a60dcd7df518911b77ec4281210d6222bfab83
william20047 <a.william.20047@gmail.com>
2025-11-27 17:34:50 -0500

    Added settings to the prompt

--- src/generate/generate.ts
+++ src/generate/generate.ts
@@ -1,17 +1,50 @@
+import { ZodError } from "zod";
 import Prompt from "./prompt";
+import { mergeSettings } from "./settings";
+import { Settings, SettingsAddition } from "./settings/internal";
+import { SettingsAdditionInputSchema, SettingsSchema } from "./settings/user";
 
 
 class Generate{
   // static features: ;
 
   prompt: Prompt;
+  settings: Settings;
 
   constructor(){
     this.prompt = new Prompt();
+    this.settings = {};
   }
 
+  /**
+   * Updates the settings for the current instance by validating and parsing
+   * the provided settings object against the defined schema.
+   *
+   * @param Settings - The settings object to be applied.
+   *
+   * @throws {ZodError} If the provided settings object does not conform to the settings format.
+   */
+  setSettings(Settings: Settings){
+    const parsedSettings = SettingsSchema.parse(Settings);
+    this.settings = parsedSettings;
+  }
 
-  const features = 
+  /**
+   * Edits the current settings by merging the provided partial settings with the existing ones.
+   * 
+   * - Any settings value explicitly set to `unset` will be removed from the existing settings.
+   * - The input settings must conform to the settings format.
+   * 
+   * @param settingsAddition - A partial object of type `SettingsAddition` containing the new settings to merge.
+   * @throws {ZodError} If the provided settings do not match the `SettingsAdditionInputSchema`.
+   */
+  editSettings(settingsAddition: Partial<SettingsAddition>){
+    const parsedNewSettings = SettingsAdditionInputSchema.parse(settingsAddition);
+    
+    const mergedSettings =  mergeSettings(this.settings, parsedNewSettings);
+    this.settings = mergedSettings;
+  }
+
 }
 
 export default Generate;

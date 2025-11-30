7f21886b942d7a1b637456db73618e7b2279d19e
william20047 <a.william.20047@gmail.com>
2025-11-29 19:30:00 -0500

    Made generate output class

---
+++ src/text-generation/generate-result.ts
@@ -0,0 +1,12 @@
+import Generate from "./generate";
+import { Message } from "./prompt/internal";
+
+class GenerateResult {
+  content : Message;
+  
+
+  constructor(generate: Generate, content: Message) {
+    this.content = content;
+
+  }
+}

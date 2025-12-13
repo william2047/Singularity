cfd866c40a055f2a628a5cb400d887bc4fa26d9d
william20047 <a.william.20047@gmail.com>
2025-12-13 18:34:10 -0500

    Removed duplicate tsup configs by importing root

---
+++ tsup.base.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig((options) => ({
+  format: ["esm", "cjs"],
+  dts: true,
+  sourcemap: true,
+  clean: true,
+  splitting: false,
+  target: "es2020",
+
+  // Allow per-package entry override
+  entry: options.entry
+}));

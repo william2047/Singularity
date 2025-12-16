b71919e7f1257f3db5f1edc6d296dd345555cdd7
william20047 <a.william.20047@gmail.com>
2025-12-16 15:46:38 -0500

    Made a base gemini model adapter

--- packages/generate/models/gemini/src/index.ts
+++ packages/generate/models/gemini/src/index.ts
@@ -1,10 +1,86 @@
-import { GenerateModel } from "@generate/core";
+import Generate, { GenerateModel } from "@generate/core";
+import GenerateOutput from "../../../core/src/output";
+import { Content } from "../../../core/src/prompt/internal";
 
-type GeminiModelIds = "gemini-1" | "gemini-1.5" | "gemini-2";
 
-class GeminiModel extends GenerateModel<GeminiModelIds> {
-  providerId = "gemini";
+function contentToGeminiPart(content: Content): any{
+  switch(content.type){
+    case "text":
+      return { text: content.value };
 
+    default:
+      throw new Error(`Unsupported content type: ${content.type}`);
+  }
 }
 
-export default GeminiModel;
+function geminiPartToContent(part: any): Content {
+  if(part.text){
+    return {
+      type: "text",
+      value: part.text,
+    }
+  }
+
+  throw new Error(`Unsupported Gemini part: ${JSON.stringify(part)}`);
+}
+  
+
+function geminiBody(generate: Generate) {
+  return {
+    contents : generate.prompt.messages.map((msg) => ({
+      role: msg.role,
+      parts: msg.content.map(part => contentToGeminiPart(part)),
+    })),
+  };
+}
+
+
+
+const modelIds = ["gemini-2.0-flash-lite","gemini-2.5-flash"] as const
+
+export class GeminiModel extends GenerateModel<typeof modelIds> {
+  providerId = "gemini";
+  readonly modelIds = modelIds;
+
+  apiKey: string;
+
+  constructor(model: typeof modelIds[number], apiKey: string) {
+    super(model);
+    this.apiKey = apiKey;
+  }
+
+  async generateInternal(generate: Generate): Promise<GenerateOutput> {
+    const body = geminiBody(generate);
+
+    const request = {
+      method: "POST",
+      headers: {
+      "x-goog-api-key": this.apiKey,
+      "Content-Type": "application/json",
+      },
+      body: JSON.stringify(geminiBody(generate)),
+    };
+
+    const req = await fetch(
+      `https://generativelanguage.googleapis.com/v1beta/models/${this.model}:generateContent`,
+      request
+    );
+
+    if (!req.ok) {
+      throw new Error(`Gemini API request failed with status ${req.status}: ${await req.text()}`);
+    }
+
+    const res = await req.json();
+
+
+    return {
+      modelId: this.model,
+      providerId: this.providerId,
+      providerData: {
+        request: body,
+        response: res
+      },
+      message: res.candidates[0].content.parts.map((part: any) => geminiPartToContent(part)),
+    }
+  }
+}

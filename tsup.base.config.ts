890bf3b083e7dc457fdba6babb86ea75e8bcb5ee
william20047 <a.william.20047@gmail.com>
2025-12-16 10:56:01 -0500

    Remade configs

--- tsup.base.config.ts
+++ tsup.base.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
   format: ["esm", "cjs"],
-  dts: true,
   sourcemap: true,
   clean: true,
   splitting: false,
   target: "es2020",
+
+  tsconfig: "tsconfig.json",
+  dts: true,
 
   // Allow per-package entry override
   entry: options.entry || ["src/index.ts"],

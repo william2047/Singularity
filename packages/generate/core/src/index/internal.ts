ba726fe839b59bb32def6d4b397713b52b3734fd
william20047 <a.william.20047@gmail.com>
2025-12-17 16:17:10 -0500

    Made adequate indexes (slowly adding to it)

--- packages/generate/core/src/index/internal.ts
+++ packages/generate/core/src/index/internal.ts
@@ -1,5 +1,18 @@
-import GenerateModel from "../model";
+import Generate from "../generate";
+import { ModelFeatureSupportRecord, FeatureHandlers } from "../features/";
+import { GenerateModel} from "../model";
+import { } from "../settings";
+import { Prompt } from "../prompt"
+import { Message, Content } from "../prompt/internal"
+import GenerateOutput from "../output"
 
 export {
+  Generate,
   GenerateModel,
+  type Prompt,
+  type Message,
+  type Content,
+  type GenerateOutput,
+  type ModelFeatureSupportRecord,
+  type FeatureHandlers,
 }
--- packages/generate/core/src/index/public.ts
+++ packages/generate/core/src/index/public.ts
@@ -1,2 +1,7 @@
 import Generate from "../generate";
+import * as Factory from "../generate-factory";
+
 export default Generate;
+export {
+  Factory
+}

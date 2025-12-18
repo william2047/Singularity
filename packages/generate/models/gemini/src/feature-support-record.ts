6b10a98189d6a1fc83cc8cfc586feceb98072d23
william20047 <a.william.20047@gmail.com>
2025-12-18 17:51:26 -0500

    Cleared unnecessary features for now

--- packages/generate/models/gemini/src/feature-support-record.ts
+++ packages/generate/models/gemini/src/feature-support-record.ts
@@ -4,10 +4,6 @@ import { ModelFeatureSupportRecord } from "@generate/core/internal";
 const FeatureSupportRecord: ModelFeatureSupportRecord = {
   "inputContent.typeText": true,
   "settings.temperature": true,
-  "settings.maxTokens": true,
-  "settings.topP": true,
-  "settings.frequencyPenalty": true,
-  "settings.presencePenalty": true,
 }
 
 export default FeatureSupportRecord;

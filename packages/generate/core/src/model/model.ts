577eacc153f2fa6e970cec75a972418a203625dd
william20047 <a.william.20047@gmail.com>
2025-12-18 14:43:35 -0500

    Implemented feature handling.

--- packages/generate/core/src/model/model.ts
+++ packages/generate/core/src/model/model.ts
@@ -1,9 +1,44 @@
-import { PartialFeatureHandlers, ModelFeatureSupportRecord, mergeFeatureHandlers, defaultFeatureHandlers } from "../features";
+import { PartialFeatureHandlers, ModelFeatureSupportRecord, mergeFeatureHandlers, defaultFeatureHandlers, FeatureHandlers, FeatureRecord, FeatureHandler } from "../features";
 import Generate from "../generate";
 import GenerateOutput from "../output";
 
 
+function handleFeatures(
+  generate: Generate,
+  featureHandler: FeatureHandlers,
+  featureSupportRecord: ModelFeatureSupportRecord
+) {
+  const usedFeatures = generate.getUsedFeatures();
 
+  for (const featureId of usedFeatures) {
+    const isSupported = featureSupportRecord[featureId] ?? false;
+
+    if(isSupported) continue;
+
+    const onUnsupported = featureHandler[featureId].onUnsupported;
+
+    // If error, throw error
+    if(onUnsupported ===  "error"){
+      throw new Error(`Feature ${featureId} is not supported by this model.`);
+    }
+
+    // If ignore, call the onIgnore function. If not defined, throw error
+    else if(onUnsupported === "ignore"){
+      const feature = FeatureRecord[featureId];
+      if ('onIgnore' in feature && typeof feature.onIgnore === 'function') {
+        feature.onIgnore(generate);
+      } else{
+        throw new Error(`Feature ${featureId} does not have an onIgnore handler.`);
+      }
+    }
+
+    // If fallback, call the fallback function
+    else if(onUnsupported === "fallback"){
+      const feature = featureHandler[featureId];
+      feature.fallbackFn(generate)
+    }
+  }
+}
 
 
 abstract class GenerateModel{
@@ -21,13 +56,16 @@ abstract class GenerateModel{
     // Cascade merge feature handlers: default handlers < model default handlers < provided handlers
     this.featureHandler = mergeFeatureHandlers(
       defaultFeatureHandlers,
-      this.featureHandler,
       featureHandler ?? {}
     )
   }
 
   async generate(generate: Generate): Promise<GenerateOutput> {
 
+    const effectiveGenerate = generate.clone();
+    handleFeatures(effectiveGenerate, this.featureHandler, this.featureSupportRecord);
+    
+    
     return this.generateInternal(generate);
   }
   
